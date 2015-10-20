41877ca32d8b0425ddc572534a9fa484eee03cf9
Nikolay Mishin <mi@ya.ru>
2015-10-21 00:35:41 +0400

    add adaptor for genopro

--- script/read_config.pl
+++ script/read_config.pl
@@ -1,3 +1,4 @@
+use v5.10;
 use Data::Show;
 my $file = $ARGV[0] or die "Need to get CSV file on the command line\n";
 $slurp_text = slurp($file);
@@ -11,4 +12,19 @@ sub slurp {
 
 my %contents_of = do { local $/; "", split /\[(\S+)\]\n/, $slurp_text };
 
-show %contents_of;
+my $Individuals=$contents_of{Individuals};
+my $Contacts=$contents_of{Contacts};
+my $Educations=$contents_of{Educations};
+my $Families=$contents_of{Families};
+my $Marriages=$contents_of{Marriages};
+my $Occupations=$contents_of{Occupations};
+my $Pictures=$contents_of{Pictures};
+my $Places=$contents_of{Places};
+my $SourcesAndCitations=$contents_of{SourcesAndCitations};
+my @Individuals_lines=split /\n/,$Individuals;
+my @column_names=split /\t/,$Individuals_lines[0];
+say "@column_names";
+# my $len=@Individuals_lines+0;
+# say "@Individuals_lines";#[l..$len];
+# show $Individuals;#%contents_of;
+show @Individuals_lines;#%contents_of;
