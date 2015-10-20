ce6277c70827914239da8f2ce7a506f400394fd5
Nikolay Mishin <mi@ya.ru>
2015-10-20 17:33:43 +0300

    read genopro csv format done!

--- script/read_config.pl
+++ script/read_config.pl
@@ -1,20 +1,14 @@
-# In your program
-use Config::Tiny;
- 
-# Create a config
-my $Config = Config::Tiny->new;
- 
-# Open the config
+use Data::Show;
 my $file = $ARGV[0] or die "Need to get CSV file on the command line\n";
-$slurp_text=slurp($file);
-sub slurp{
-my ($abs_file_path)=@_;
-open my $fh, '<', $abs_file_path or croak $!;
+$slurp_text = slurp($file);
+
+sub slurp {
+    my ($abs_file_path) = @_;
+    open my $fh, '<', $abs_file_path or croak $!;
     local $/;
     my $text = <$fh>;
-	}
-# $Config = Config::Tiny->read( $file );
-# use Data::Dumper;
-# print Dumper $Config;
+}
 
-my %contents_of = do { local $/; "", split /\[ (\S+) \]_+\n/, $slurp_text };
+my %contents_of = do { local $/; "", split /\[(\S+)\]\n/, $slurp_text };
+
+show %contents_of;
