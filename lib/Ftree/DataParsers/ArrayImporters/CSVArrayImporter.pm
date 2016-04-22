8db8d2ffd6fb3614303407be4dafc2c63058814f
Nikolay Mishin <nmishin@masterdm.ru>
2016-04-22 19:22:54 +0300

    fix some perlcritic error

--- lib/Ftree/DataParsers/ArrayImporters/CSVArrayImporter.pm
+++ lib/Ftree/DataParsers/ArrayImporters/CSVArrayImporter.pm
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# For a copy of the GNU General Public License, visit 
+# For a copy of the GNU General Public License, visit
 # http://www.gnu.org or write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
@@ -26,15 +26,16 @@ use warnings;
 use version; our $VERSION = qv('2.3.39');
 use Params::Validate qw(:all);
 # use CGI::Carp qw(fatalsToBrowser);
+my $fh;
 
 sub new {
     my ($classname, $file_name, $encoding) = @_;
 	my $self = {
     	current_line => undef,
   };
-  open DATAFILE, "<:encoding($encoding)", "$file_name" 
+  open $fh, "<:encoding($encoding)", "$file_name"
      or die "Unable to open datafile $file_name";
-  $self->{current_line} = <DATAFILE>;
+  $self->{current_line} = <$fh>;
   return bless $self, $classname;
 }
 sub hasNext {
@@ -45,17 +46,17 @@ sub next {
 	my ($self) = validate_pos(@_, {type => HASHREF});
 	my $prevline = $self->{current_line};
 	do {
-		$self->{current_line} = <DATAFILE>;		
+		$self->{current_line} = <$fh>;
 	}
 	while($self->{current_line} && $self->{current_line} =~ m/^\s*\#/x);  # skip comments
 
 
-	chomp($prevline); 
+	chomp($prevline);
 	return split( /;/, $prevline);
 }
 sub close {
 	my ($self) = validate_pos(@_, {type => HASHREF});
-	close(DATAFILE) or die "Unable to close datafile";
+	close($fh) or die "Unable to close datafile";
 }
- 
+
 1;
